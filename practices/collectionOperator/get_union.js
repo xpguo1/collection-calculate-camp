d5d418d520a71f795efd39d6f008181af8dac929
xpguo1 <xpguo@thoughtworks.com>
2019-01-17 19:07:21 +0800

    pass get_union_spec

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,20 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  var a = 0;
+  var b = 0;
+  var count = 0;
+  for(b; b < collection_b.length; b++) {
+    for(a; a < collection_a.length; a++){
+      if(collection_b[b] != collection_a[a])
+        count++;
+    }
+    if (count === collection_a.length)
+      collection_a.push(collection_b[b]);
+    a = 0;
+    count = 0;
+  }
+  return collection_a;
 }
 
 module.exports = get_union;
