9b64ce4fa4c3e09c9901ba78a67a40777af599c1
xpguo1 <xpguo@thoughtworks.com>
2019-01-17 18:02:12 +0800

    pass get_integer_interval_spec

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,16 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  var collect = [];
+  while (number_a != number_b){
+    collect.push(number_a);
+    if(number_a > number_b)
+      number_a--;
+    else if(number_a < number_b)
+      number_a++;
+  }
+  collect.push(number_a);
+  return collect;
 }
 
 module.exports = get_integer_interval;
