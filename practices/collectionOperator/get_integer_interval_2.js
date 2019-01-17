79cd75b3addc726cda950c95e7bff90a50522c86
xpguo1 <xpguo@thoughtworks.com>
2019-01-17 17:56:21 +0800

    pass get_integer_interval_2

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,20 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+
+  var collect = [];
+  while (number_a != number_b){
+    if(number_a % 2 === 0)
+      collect.push(number_a);
+    if(number_a > number_b)
+      number_a--;
+    else if(number_a<number_b)
+      number_a++;
+  }
+  if(number_a % 2 === 0)
+    collect.push(number_a);
+
+  return collect;
 }
 
 module.exports = get_integer_interval_2;
