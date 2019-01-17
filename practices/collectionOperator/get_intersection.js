8cda7d19dd04c4cabede113352312be595ba76d6
xpguo1 <xpguo@thoughtworks.com>
2019-01-17 18:25:09 +0800

    pass get_intersection_spec

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,6 +2,17 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  var a = 0;
+  var b = 0;
+  var collect = [];
+  for(b; b < collection_b.length; b++) {
+    for(a; a < collection_a.length; a++){
+      if(collection_a[a] === collection_b[b])
+        collect.push(collection_b[b]);
+    }
+    a = 0;
+  }
+  return collect;
 }
 
 module.exports = get_intersection;
