caf126e86d5fef2fd7449d93d34c69380062277e
Xuepei Guo <xpguo@CNxpguo.local>
2019-01-17 16:16:23 +0800

    pass collect_all_even

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function collect_all_even(collection) {
+  // [1, 2, 3, 4, 5]
   //在这里写入代码
+  return collection.filter(result => result % 2 ===0);
+
 }
 
 module.exports = collect_all_even;
