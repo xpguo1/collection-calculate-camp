317c8c42b5414c297092972e849f85c3e8aa828d
xpguo1 <xpguo@thoughtworks.com>
2019-01-21 17:35:02 +0800

    pass choose_even_spec.js

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,15 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  var collect = [];
+  var letter;
+  while(number_a != number_b){
+    //letter = String.fromCharCode(number_a + 96);
+    collect.push(number_a + 96);
+  }
+ // letter = String.fromCharCode(number_a + 96);
+  collect.push(number_a + 96);
+  return collect;
 }
 
 module.exports = get_letter_interval;
