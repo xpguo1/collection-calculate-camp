26ff5c997f010d57af5ce05d0ea2328b8489d5c7
xpguo1 <xpguo@thoughtworks.com>
2019-01-23 19:13:54 +0800

    pass two_collections_practice_3_spec

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,7 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  return collection_a.filter(result => collection_b.find(v => result % v === 0));
 }
 
 module.exports = choose_divisible_integer;
