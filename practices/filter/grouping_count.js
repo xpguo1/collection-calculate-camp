cf418fb78ea74d03c1391eee4705e28c0b3cf516
xpguo1 <xpguo@thoughtworks.com>
2019-01-23 18:51:33 +0800

    pass grouping_count_spec

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,16 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+
+  var sum;
+  var map = {}; //object和array的区别，对象的输出格式为{key:value…… },数组的输出格式为[element……]
+  collection.forEach(function (element,index,self) { //mapping自定义返回格式，foreach无返回值，filter只返回符合条件的数组元素
+    if(self.indexOf(element) === index) {
+      sum = collection.filter(arr => arr == element).length;
+      map[element] = sum;
+    }
+  });
+  return map;
 }
 
 module.exports = grouping_count;
