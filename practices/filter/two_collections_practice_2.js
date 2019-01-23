562e4531bfe48e3048edb7249339f7f3da0581b2
xpguo1 <xpguo@thoughtworks.com>
2019-01-23 19:02:18 +0800

    pass two_collections_practice_2_spec

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,6 +3,7 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  return collection_a.concat(collection_b).filter(result => !collection_b.includes(result) || !collection_a.includes(result));
 }
 
 module.exports = choose_no_common_elements;
