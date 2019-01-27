520051bbcdab48d32aae4c6b66918be942c851da
xpguo1 <xpguo@thoughtworks.com>
2019-01-27 20:16:23 +0800

    pass specs/reduce/find_element_last_subscript_spec.js

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -2,10 +2,8 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
-return collection.findIndex(function (elem,index,self) {
-  if (elem === element && self.indexOf(elem) === index)
-    return index;
-});
+return collection.indexOf(element);
+
 }
 
 module.exports = calculate_elements_sum;
