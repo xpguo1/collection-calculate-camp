d4760bb917434992e34f3a0634bfda2567723013
xpguo1 <xpguo@thoughtworks.com>
2019-01-27 18:56:43 +0800

    pass specs/reduce/compare_collections_spec

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -2,6 +2,7 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.sort().toString() == collection_b.sort().toString();
 }
 
 module.exports = compare_collections;
