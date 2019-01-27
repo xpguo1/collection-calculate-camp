32b715009cbede1ab45daddccb077629fcbe1e08
xpguo1 <xpguo@thoughtworks.com>
2019-01-27 18:19:38 +0800

    pass double_dimensional_array_to_one_2_spec

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,9 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  return [].concat.apply([],collection).filter(function (element,index,self) {
+    return self.indexOf(element) === index;
+  });
 }
 
 module.exports = double_to_one;
