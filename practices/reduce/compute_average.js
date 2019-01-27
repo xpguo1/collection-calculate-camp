b036283f5e0ca8f262c7887fc6190c10195e3309
xpguo1 <xpguo@thoughtworks.com>
2019-01-27 19:03:18 +0800

    specs/reduce/compute_average_spec.js

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,6 +2,7 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  return collection.reduce((previous,current) => current += previous)/collection.length;
 }
 
 module.exports = compute_average;
