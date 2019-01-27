31fc90434ab0e4681d51ddb3fc39227a8310df1b
xpguo1 <xpguo@thoughtworks.com>
2019-01-27 18:41:42 +0800

    pass number_map_to_word_spec

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,7 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(x => String.fromCharCode(x + 96));
+
 };
 
 module.exports = number_map_to_word_over_26;
