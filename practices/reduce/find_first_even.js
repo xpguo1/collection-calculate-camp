669ebf23ce77d925630ea5f19f2eafc3c4aaa634
xpguo1 <xpguo@thoughtworks.com>
2019-02-12 13:46:43 +0800

    pass split_to_zero_spec.js

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,6 +2,9 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  return collection.reduce((acc,cur)=>)
+
+
 }
 
 module.exports = find_first_even;
--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,12 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var collect = [];
+  collect.push(number);
+  while(collect[collect.length-1]>0){
+    collect.push((collect[collect.length-1]*10 - interval*10)/10);
+  }
+  return collect;
 }
 
 module.exports = spilt_to_zero;
