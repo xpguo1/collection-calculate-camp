fc3ed9e9fd2cf32a798ccfe0491966f3f7d2dd05
xpguo1 <xpguo@thoughtworks.com>
2019-02-12 13:52:03 +0800

    pass find_last_even_spec.js

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,8 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  var collect = collection.filter(result => result % 2 == 0);
+  return collect[collect.length-1];
 }
 
 module.exports = find_last_even;
