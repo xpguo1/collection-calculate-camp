602d3b02a6f87b24966f0f73f343c677c5fe6e3e
xpguo1 <xpguo@thoughtworks.com>
2019-02-13 18:19:04 +0800

    pass compute_median_spec.js

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,9 +2,14 @@
 
 function compute_median(collection) {
   //在这里写入代码
-  if (collection.length % 2 == 0)
+  var collect = collection.sort((a,b) => a - b);
+  if (collect.length % 2 != 0)
   {
+    return collect[(collect.length-1)/2];
+  }
 
+  else {
+    return (collect[collect.length/2]+collect[(collect.length)/2-1])/2;
   }
 }
 
