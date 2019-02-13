ca3a53485c82a8f4f3c3930273f4c04d7180ec8c
xpguo1 <xpguo@thoughtworks.com>
2019-02-13 18:32:26 +0800

    pass compute_chain_median_spec.js

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,12 +2,9 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
-  var collect = [];
-  collect = collection.split('->');
-  collect.sort(function (v1,v2) {
-    return v1<v2?1:-1;});
-  console.log(collect);
-  return collect[collect.length%2==0? collect.length/2:(collect.length+1)/2];
+  var collect = collection.split('->').sort((a,b) => a-b).map(Number);
+
+  return collect.length % 2 === 0 ? (collect[collect.length / 2] + collect[collect.length / 2 - 1])/2 : collect[(collect.length-1)/2];
 
 }
 
