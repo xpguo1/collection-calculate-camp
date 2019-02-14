f838da2745559e8fe232a12a07c4f7f607ca6dcc
xpguo1 <xpguo@thoughtworks.com>
2019-02-14 13:39:01 +0800

    pass practice_2_spec

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,17 @@
 function count_same_elements(collection) {
   //在这里写入代码
+
+  var collect = collection.filter(function(elem,index,arr){
+    return arr.indexOf(elem) === index
+  });
+  var collect2 = [];
+  collect.map(function (element) {
+    var object1 = new Object();
+    object1['key'] = element;
+    object1['count'] = collection.reduce((a, v) => v === element ? a + 1 : a + 0, 0);
+    collect2.push(object1);
+  });
+  return collect2;
 }
 
 module.exports = count_same_elements;
