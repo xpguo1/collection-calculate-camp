e4eedf3b2d4cb0f6beb23c0a9db401a61a162523
xpguo1 <xpguo@thoughtworks.com>
2019-02-20 14:26:00 +0800

    if(x%26 === 0){

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -3,14 +3,10 @@
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
   var collect = [];
-  var letter;
-  while(number_a != number_b){
-    //letter = String.fromCharCode(number_a + 96);
-    collect.push(number_a + 96);
+  for (var x = Math.min(number_a,number_b);x<= Math.max(number_a,number_b);x++){
+    collect.push(x => x < 26 ? String.fromCharCode(x + 96):String.fromCharCode(x/26 + 96)+String.fromCharCode(x%26+96))
   }
- // letter = String.fromCharCode(number_a + 96);
-  collect.push(number_a + 96);
-  return collect;
+  return number_a>number_b ? collect.sort((a,b)=>b-a):collect.sort((a,b)=>a-b);
 }
 
 module.exports = get_letter_interval;
--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -3,10 +3,28 @@
 function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
   var collect = [];
-  for (var x = Math.min(number_a,number_b);x<= Math.max(number_a,number_b);x++){
-    collect.push(x => x < 26 ? String.fromCharCode(x + 96):String.fromCharCode(x/26 + 96)+String.fromCharCode(x%26+96))
+  if(number_a<number_b) {
+    for (var x = number_a; x <= number_b; x++) {
+      if(x%26 === 0){
+        collect.push(x === 26?String.fromCharCode(x+96):String.fromCharCode(x/26-1+96)+String.fromCharCode(26+96))
+      }
+      else {
+        collect.push(x < 26 ? String.fromCharCode(x + 96) : String.fromCharCode(x / 26 + 96) + String.fromCharCode(x % 26 + 96))
+      }
+    }
   }
-  return number_a>number_b ? collect.sort((a,b)=>b-a):collect.sort((a,b)=>a-b);
+  else if (number_a >= number_b){
+    for (var x = number_a; x >= number_b; x--) {
+      if(x%26 === 0){
+        collect.push(x === 26?String.fromCharCode(x+96):String.fromCharCode(x/26-1+96)+String.fromCharCode(26+96))
+      }
+      else {
+        collect.push(x < 26 ? String.fromCharCode(x + 96) : String.fromCharCode(x / 26 + 96) + String.fromCharCode(x % 26 + 96))
+      }
+    }
+  }
+
+  return collect;
 }
 
 module.exports = get_letter_interval_2;
